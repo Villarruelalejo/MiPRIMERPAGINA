4798d82325cdf5524a79f40a9a6d378d62f71b65
Villarruelalejo <135070955+Villarruelalejo@users.noreply.github.com>
2023-05-30 19:34:10 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const miBoton = document.getElementById('miBoton');
+function handleClick(){
+    alert('¡Hola! Ha hecho clic en el botón.');
+}
+addEventListener
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
